47f62e09945d678b732929f9c1caf9ce6e18be37
a31859 <a31859@users.noreply.github.com>
2020-01-07 10:51:13 +0000

    feat(update-script): script update to build the changed services and better automize the build process

--- docker/build_push_docker.sh
+++ docker/build_push_docker.sh
@@ -1,33 +1,52 @@
 #!/usr/bin/env bash
 
-# remove .env if exits
+###################################################################
+# Script Name : build_push_docker.sh
+# Description	: Will create the necessary environment variables, \
+#               identify the services to build (monorepo) and call docker compose to \
+#               build and push for those services
+# Args        : -
+# Author      : Délio Amaral
+# Email       : delio@cloudoki.com
+###################################################################
+
+# Function to convert yaml to json with ruby
+function yaml2json()
+{
+  ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' $*
+}
+
+# Remove .env if exits
 rm .env
 
-# login into docker hub
+# Login into docker hub
 echo ${DOCKER_USER_PASS} | docker login --username ${DOCKER_USER_NAME} --password-stdin
 
-# create .env
+# Create .env
 touch .env
 
-# docker repo
+# Set docker repo
 DOCKER_REPO="delioamaral/circleci-test"
 
-# create env vars with the version for each package
-HELLO_PACKAGE_VERSION="develop"
-HELLO_2_PACKAGE_VERSION="develop"
+# Set the root directory of projects folder
+ROOT_PROJECTS_FOLDER="packages"
 
-if [ "${CIRCLE_BRANCH}" != "develop" ]; then
-  HELLO_PACKAGE_VERSION=$(cat ../packages/hello/package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
-  HELLO_2_PACKAGE_VERSION=$(cat ../packages/hello2/package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
-fi
+# Set the list of projects
+PROJECTS_LIST=($(ls -d ../${ROOT_PROJECTS_FOLDER}/*/ | cut -f3 -d/))
 
-# write envs to .env
+# Write envs to .env
 echo "DOCKER_REPO=${DOCKER_REPO}" >> .env
-echo "HELLO_PACKAGE_TAG=hello-${HELLO_PACKAGE_VERSION}" >> .env
-echo "HELLO_2_PACKAGE_TAG=hello2-${HELLO_2_PACKAGE_VERSION}" >> .env
+
+# Create env vars with the version for each package and write to .env
+for project in ${PROJECTS_LIST}; do
+  PROJECT_PACKAGE_VERSION="develop"
+  if [ "${CIRCLE_BRANCH}" != "develop" ]; then
+    PROJECT_PACKAGE_VERSION=$(cat ../${ROOT_PROJECTS_FOLDER}/${project}/package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
+  fi
+  echo "$(echo $project | tr '[:lower:]' '[:upper:]' | tr '-' '_')_PACKAGE_TAG=${project}-${PROJECT_PACKAGE_VERSION}" >> .env
+done
 
 # Identify modified directories
-
 ## Get latest succesful build/commit
 LAST_SUCCESSFUL_BUILD_URL="https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH?filter=completed&limit=1"
 LAST_SUCCESSFUL_COMMIT=`curl -Ss -u "$CIRCLE_TOKEN:" $LAST_SUCCESSFUL_BUILD_URL | jq -r '.[0]["vcs_revision"]'`
@@ -38,10 +57,26 @@ if [[ ${LAST_SUCCESSFUL_COMMIT} == "null" ]]; then
 else
   COMMITS="${CIRCLE_SHA1}..${LAST_SUCCESSFUL_COMMIT}"
 fi
-git diff --name-only $COMMITS | cut -d/ -f1 | sort -u > projects
-echo -e "Modified directories:\n`cat projects`\n"
+# Filter result and only list the project folders that where updated
+PROJECTS=$(git diff --name-only origin/$COMMITS | grep "${ROOT_PROJECTS_FOLDER}" | cut -d/ -f2 | sort -u)
+echo -e "Modified directories:\n`echo ${PROJECTS}`\n"
 
-# build and push to docker registry
+# Get the projects folder name to build
+PARSED_YAML=$(cat docker-compose.yaml | yaml2json)
+for project in ${PROJECTS}; do
+  if [[ " ${PROJECTS_LIST[@]} " =~ " ${project} " ]]; then
+    SERVICE_NAME=$(echo $PARSED_YAML | jq -r ".services | to_entries[] | select (.value.build == \"../${ROOT_PROJECTS_FOLDER}/${project}\") | .key")
+    echo "Will build [folder, service] -> [$project, $SERVICE_NAME]"
+    DOCKER_SERVICES="$DOCKER_SERVICES $SERVICE_NAME"
+  fi
+done
+
+echo "Services to build: $DOCKER_SERVICES"
+
+# Build and push to docker registry
 docker-compose config
-# docker-compose build
-# docker-compose push
+# docker-compose build $DOCKER_SERVICES
+# docker-compose push $DOCKER_SERVICES
+
+# Logout from docker
+# docker logout
