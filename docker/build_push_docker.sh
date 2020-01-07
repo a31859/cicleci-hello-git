b5c637861bca82d406952f3defa8c29dc75e30cf
a31859 <a31859@users.noreply.github.com>
2020-01-07 13:05:44 +0000

    fix(script): projects list as array to fix loops

--- docker/build_push_docker.sh
+++ docker/build_push_docker.sh
@@ -32,7 +32,8 @@ DOCKER_REPO="delioamaral/circleci-test"
 ROOT_PROJECTS_FOLDER="packages"
 
 # Set the list of projects
-PROJECTS_LIST=($(ls -d ../${ROOT_PROJECTS_FOLDER}/*/ | cut -f3 -d/))
+PROJECTS_LIST=$(ls -d ../${ROOT_PROJECTS_FOLDER}/*/ | cut -f3 -d/)
+echo $PROJECTS_LIST
 
 # Write envs to .env
 echo "DOCKER_REPO=${DOCKER_REPO}" >> .env
@@ -43,6 +44,7 @@ for project in ${PROJECTS_LIST}; do
   if [ "${CIRCLE_BRANCH}" != "develop" ]; then
     PROJECT_PACKAGE_VERSION=$(cat ../${ROOT_PROJECTS_FOLDER}/${project}/package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
   fi
+  echo "Creating the var for $(echo $project | tr '[:lower:]' '[:upper:]' | tr '-' '_')_PACKAGE_TAG"
   echo "$(echo $project | tr '[:lower:]' '[:upper:]' | tr '-' '_')_PACKAGE_TAG=${project}-${PROJECT_PACKAGE_VERSION}" >> .env
 done
 
@@ -57,14 +59,21 @@ if [[ ${LAST_SUCCESSFUL_COMMIT} == "null" ]]; then
 else
   COMMITS="${CIRCLE_SHA1}..${LAST_SUCCESSFUL_COMMIT}"
 fi
-# Filter result and only list the project folders that where updated
-PROJECTS=$(git diff --name-only $COMMITS | grep "${ROOT_PROJECTS_FOLDER}" | cut -d/ -f2 | sort -u)
+# Filter result and only list the project folders that where updated $COMMITS
+PROJECTS=$(git diff --name-only origin/master | grep "${ROOT_PROJECTS_FOLDER}" | cut -d/ -f2 | sort -u)
 echo -e "Modified directories:\n`echo ${PROJECTS}`\n"
 
 # Get the projects folder name to build
+# Parse the ymal to json
 PARSED_YAML=$(cat docker-compose.yaml | yaml2json)
+# Convert the project list to an array
+PROJECTS_AS_ARRAY=($(echo ${PROJECTS_LIST}))
 for project in ${PROJECTS}; do
-  if [[ " ${PROJECTS_LIST[@]} " =~ " ${project} " ]]; then
+  # Remove the project from the array if exist
+  CLEANED_PROJECTS_ARRAY=(${PROJECTS_AS_ARRAY[@]/#$project})
+
+  # Compare the arrays sizes to see if theres a match (different sizes is a match)
+  if [[ "${#PROJECTS_AS_ARRAY[@]}" != "${#CLEANED_PROJECTS_ARRAY[@]}" ]]; then
     SERVICE_NAME=$(echo $PARSED_YAML | jq -r ".services | to_entries[] | select (.value.build == \"../${ROOT_PROJECTS_FOLDER}/${project}\") | .key")
     echo "Will build [folder, service] -> [$project, $SERVICE_NAME]"
     DOCKER_SERVICES="$DOCKER_SERVICES $SERVICE_NAME"
